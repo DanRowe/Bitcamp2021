671d16f8783aa03f8f4210946bbf678943a6c714
Andrew Knox <amknox5@gmail.com>
2021-04-11 11:02:25 -0400

    reading cocktail names from json based on the week

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv");
-const PythonShell = require("python-shell");
-const { spawn } = require("child_process");
 let result = dotenv.config();
+const fs = require("fs");
 
 // only run if today is Friday
 if (new Date().getDay() === 5) {
@@ -16,47 +15,37 @@ if (new Date().getDay() === 5) {
     strictSSL: true, // optional - requires SSL certificates to be valid.
   });
 
-  const python = spawn("python", ["webscraper.py"]);
-  let output = [];
-  python.stdout.on("data", function (data) {
-    output.push(data);
-  });
-  let response = "";
-  let name = "";
-  python.on("close", (code) => {
-    m = output.join("").match(/.{1,141}\./g);
-    for (let i = 0; i < m.length; i++) {
-      console.log(`(${i + 1}/${m.length})` + m[i]);
+  let cocktail = {};
 
-      if (i == 0) {
-        sleep(4000);
-        T.post(
-          "statuses/update",
-          { status: `(${i + 1}/${m.length})` + m[i] },
-          function (err, data, response) {
-            console.log(data);
-            let out = output.join("");
-            console.log(out.length);
-            reponse = data.id_str;
-            // name = data.user
-          }
-        );
-      } else {
-        sleep(4000);
-        T.post(
-          `statuses/update`,
-          {
-            in_reply_to_status_id: "" + response,
-            status: `(${i + 1}/${m.length})` + m[i] + ` @weekendcocktai1`,
-          },
-          function (err, data, response) {
-            console.log(data);
-            let out = output.join("");
-            console.log(out.length);
-          }
-        );
-      }
+  fs.readFile("./cocktail.json", "utf8", (err, jsonString) => {
+    if (err) {
+      console.log("File read failed:", err);
+      return;
+    }
+    try {
+      const cocktails = JSON.parse(jsonString).data;
+      let now = new Date();
+      let onejan = new Date(now.getFullYear(), 0, 1);
+      let week = Math.ceil(
+        ((now.getTime() - onejan.getTime()) / 86400000 + onejan.getDay() + 1) /
+          7
+      );
+      cocktail = cocktails[week];
+      console.log(cocktail);
+      T.post(
+        "statuses/update",
+        {
+          status:
+            `${cocktail.name}\n${cocktail.detail}`,
+        },
+        function (err, data, response) {
+          console.log(data);
+        }
+      );
+    } catch (err) {
+      console.log("Error parsing JSON string:", err);
     }
   });
+  return;
 }
 console.log("Today is not Friday");
