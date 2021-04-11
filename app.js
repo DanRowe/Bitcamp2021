d7d9a6b609f3938fbb1d2ffd88f65c627f9f28fd
Daniel <drowe98@live.com>
2021-04-11 00:20:13 -0400

    only run if it's Friday

--- app.js
+++ app.js
@@ -1,24 +1,32 @@
 const dotenv = require("dotenv");
 let result = dotenv.config();
 
-var Twit = require("twit");
+// only run if today is Friday
+if (new Date().getDay() === 5) {
+  var Twit = require("twit");
 
-var T = new Twit({
-  consumer_key: process.env.API_KEY,
-  consumer_secret: process.env.API_KEY_SECRET,
-  access_token: process.env.ACCESS_TOKEN,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
-  strictSSL: true, // optional - requires SSL certificates to be valid.
-});
+  var T = new Twit({
+    consumer_key: process.env.API_KEY,
+    consumer_secret: process.env.API_KEY_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
+    strictSSL: true, // optional - requires SSL certificates to be valid.
+  });
 
-//
-//  tweet 'hello world!'
-//
-T.post(
-  "statuses/update",
-  { status: "hello world!" },
-  function (err, data, response) {
-    console.log(data);
-  }
-);
+  //
+  //  tweet 'hello world!'
+  //
+  T.post(
+    "statuses/update",
+    {
+      status:
+        "Reverse Revolver\n2 oz coffee liqueur, 0.5 oz whiskey, 2 dashes orange bitters",
+    },
+    function (err, data, response) {
+      console.log(data);
+    }
+  );
+  return;
+}
+console.log("Today is not Friday");
